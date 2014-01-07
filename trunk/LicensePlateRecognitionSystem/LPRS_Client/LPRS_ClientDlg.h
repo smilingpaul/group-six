b19322505e38251ebc782ee69454b97a761f99ca
mingenhuang@gmail.com <mingenhuang@gmail.com@d30ac609-b12e-bef9-c096-90572f33a705>
2014-01-07 05:12:35 +0000

    new

---
+++ trunk/LicensePlateRecognitionSystem/LPRS_Client/LPRS_ClientDlg.h
@@ -0,0 +1,35 @@
+
+// LPRS_ClientDlg.h : 头文件
+//
+
+#pragma once
+#include "afxwin.h"
+
+
+// CLPRS_ClientDlg 对话框
+class CLPRS_ClientDlg : public CDialogEx
+{
+// 构造
+public:
+	CLPRS_ClientDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_LPRS_CLIENT_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+
+};
